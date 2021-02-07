8ec2c6c2c8cf70b55d4c7c99f573dab3226cab6c
Ilian Ivanov <ilianvnv@yahoo.com>
2021-02-07 20:35:28 +0200

    innitial architecture

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const config = require('./config/config');
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)) 

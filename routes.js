71a6416f0d1bf4aba0a2268767e462c4793fbbe2
Ilian Ivanov <ilianvnv@yahoo.com>
2021-02-17 18:42:00 +0200

    create Accessory

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
+
 const productController = require('./controllers/productsController');
+const accessoryController = require('./controllers/accessoryController');
 const aboutController = require('./controllers/aboutController');
+
 const router = Router();
 
 router.use('/', productController);
 router.use('/about', aboutController);
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {
     res.render('404');
 })
